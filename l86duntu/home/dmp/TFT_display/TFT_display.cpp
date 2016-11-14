4c08f0dbb2203014e662924d9583459eca156a15
Jia Siang Tian <loveshawn0913@gmail.com>
2016-11-14 11:53:03 +0800

    ZaiBo
    rename this project.
    update my blog.
    put video on youtube

---
+++ l86duntu/home/dmp/TFT_display/TFT_display.cpp
@@ -0,0 +1,178 @@
+#include "Arduino.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include <sys/ioctl.h>
+#include <net/if.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#include "sio_client.h"
+#include "sio_message.h"
+#include "sio_socket.h"
+
+#include <TFT.h>
+#include <SPI.h>
+#define cs   42
+#define dc   43
+#define rst  44
+
+
+int TFT_val[4] = {0};
+int TFT_rval[4] = {0};
+char* TFT_name[4] = {"Humidity: ", "Air Humidity: ", "Temperature: ", "Pressure: "};
+char* TFT_unit[4] = {"%", "%RH", "C", "mbar"};
+
+
+float ad[5];
+int ad_pins_map[5] = {1,0,3,2,4};
+int ad_magnification[5] = {100,100,100,100,1000};
+
+
+sio::client socketio;
+
+
+
+char* get_IP(const char *iface){
+	
+	struct ifreq ifr;
+	int fd = socket(AF_INET, SOCK_DGRAM, 0);
+ 
+    //Type of address to retrieve - IPv4 IP address
+    ifr.ifr_addr.sa_family = AF_INET;
+ 
+    //Copy the interface name in the ifreq structure
+    strncpy(ifr.ifr_name , iface , IFNAMSIZ-1);
+ 
+    ioctl(fd, SIOCGIFADDR, &ifr);
+ 
+    close(fd);
+	return inet_ntoa(( (struct sockaddr_in *)&ifr.ifr_addr )->sin_addr);
+}
+
+void OnMessage(sio::event & val)
+{
+	ad[ad_pins_map[0]] = (float)val.get_message()->get_map()["Remaining"]->get_int() 	/ ad_magnification[0];
+	ad[ad_pins_map[1]] = (float)val.get_message()->get_map()["Humidity"]->get_int() 	/ ad_magnification[1];
+	ad[ad_pins_map[2]] = (float)val.get_message()->get_map()["etHumidity"]->get_int() 	/ ad_magnification[2];
+	ad[ad_pins_map[3]] = (float)val.get_message()->get_map()["Temperature"]->get_int()	/ ad_magnification[3];
+	ad[ad_pins_map[4]] = (float)val.get_message()->get_map()["Pressure"]->get_int() 	/ ad_magnification[4];
+}
+
+void TFT_drawBitmap(int x, int y, FILE *myFile, TFT *TFTdisp) {
+  if (myFile) {
+    unsigned long w = 0, h = 0;
+    char c = 0, R = 0, G = 0, B = 0;
+	for(int i = 0; i < 18 && c != EOF; i++)
+		c = getc(myFile);
+    for (int i = 0; i < 4 && c != EOF; i++)
+      w += (((c = getc(myFile)) & 0xFF) << (i * 8));
+    for (int i = 0; i < 4 && c != EOF; i++)
+      h += (((c = getc(myFile)) & 0xFF) << (i * 8));
+	for(int i = 0; i < 28 && c != EOF; i++)
+		c = getc(myFile);
+	
+	for (int j = h - 1; j >= 0; j--) {
+      for (int i = 0; i < w; i++){
+		B = getc(myFile);
+		G = getc(myFile);
+		R = getc(myFile);
+		TFTdisp->stroke(R, G, B);
+		TFTdisp->point(x+i, y+j);
+	  }
+      for (int i = 4; i > (w % 4) && (w % 4) != 0; i--)
+        getc(myFile);
+    } 
+  }
+}
+
+int main( int argc, char* args[] )
+{
+	
+	char usbwifiID[20];
+	FILE *fp = fopen("/tmp/usbwifiID", "r");
+	fscanf(fp, "%s", usbwifiID);
+	fclose(fp);
+	
+	TFT TFTscreen = TFT(cs, dc, rst);
+	TFTscreen.begin();
+	TFTscreen.background(0, 0, 0);
+	
+	
+	socketio.connect("http://localhost:3000");
+	
+	for( int count = 0; !socketio.opened() && count < 30; count ++){
+		usleep(100000);
+	}
+	
+	TFTscreen.fill(255, 0, 0);
+	TFTscreen.stroke(255, 0, 0);
+	TFTscreen.rect(0, 0, 160, 15);
+	
+	TFTscreen.stroke(255, 255, 255);
+	TFTscreen.setTextSize(1);
+	TFTscreen.text(" < 86Duino plant on TFT > ", 0, 3);
+	
+	TFTscreen.fill(0, 0, 255);
+	TFTscreen.stroke(0, 0, 255);
+	TFTscreen.rect(0, 114, 160, 15);
+	
+	TFTscreen.stroke(255, 255, 255);
+	TFTscreen.setTextSize(1);
+	TFTscreen.text(get_IP(usbwifiID), 40, 117);
+	
+	usleep(100000);
+	FILE *file = fopen("/home/dmp/TFT_display/background.bmp", "r");
+	TFT_drawBitmap(55, 15, file, &TFTscreen);
+
+	for (int i = 0; i < 4; i++) {
+		TFTscreen.stroke(0, 255, 0);
+		TFTscreen.text(TFT_name[i], 5 , 16 + i * 25);
+		TFTscreen.text(TFT_unit[i], 70 , 30 + i * 25);
+	}
+	char buffer[10];
+	
+    while( true )
+    {		
+
+		socketio.socket()->on("update", &OnMessage);
+		
+		TFT_val[0] = ad[0]* ad_magnification[0];
+		TFT_val[1] = ad[2]* ad_magnification[2];
+		TFT_val[2] = ad[3]* ad_magnification[3];
+		TFT_val[3] = ad[4]* ad_magnification[4];
+		
+		TFTscreen.setTextSize(2);
+		for (int i = 0; i < 4; i++) {
+			if(TFT_val[i] == TFT_rval[i])
+				continue;
+			/*
+			sprintf(buffer, "% 5d", TFT_rval[i]);
+			TFTscreen.stroke(0, 0, 0);
+			TFTscreen.text(buffer, 5 , 24 + i * 25);
+			*/
+			TFTscreen.fill(0, 0, 0);
+			TFTscreen.stroke(0, 0, 0);
+			TFTscreen.rect(5, 24 + i * 25, 60, 15);
+		}
+		for (int i = 0; i < 4; i++) {
+			sprintf(buffer, "% 5d", TFT_val[i]);
+			TFTscreen.stroke(0, 255, 0);
+			TFTscreen.text(buffer, 5 , 24 + i * 25);
+		}
+		
+		TFT_rval[0] = ad[0]* ad_magnification[0];
+		TFT_rval[1] = ad[2]* ad_magnification[2];
+		TFT_rval[2] = ad[3]* ad_magnification[3];
+		TFT_rval[3] = ad[4]* ad_magnification[4];
+		
+				
+		usleep(500000);
+		
+    }
+    return 0;
+}
+
+
---
+++ l86duntu/home/dmp/UI_display/UI_display.cpp
@@ -0,0 +1,525 @@
+#include "Arduino.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+#include "SDL/SDL.h"
+#include "SDL/SDL_image.h"
+#include "SDL/SDL_ttf.h"
+#include "SDL/SDL_gfxPrimitives.h"
+
+#include <sys/ioctl.h>
+#include <net/if.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#include "sio_client.h"
+#include "sio_message.h"
+#include "sio_socket.h"
+/*
+#include <TFT.h>
+#include <SPI.h>
+#define cs   10
+#define dc   9
+#define rst  8
+int TFT_val[4] = {0};
+int TFT_rval[4] = {0};
+char* TFT_name[4] = {"Humidity: ", "Air Humidity: ", "Temperature: ", "Pressure: "};
+char* TFT_unit[4] = {"%", "%RH", "C", "mbar"};
+
+*/
+short keyshift_x = 0,keyshift_y = 0;
+
+float ad[5];
+int ad_pins_map[5] = {1,0,3,2,4};
+int ad_magnification[5] = {100,100,100,100,1000};
+SDL_Surface *ad_msg = NULL;
+
+const int SCREEN_WIDTH = 320;
+const int SCREEN_HEIGHT = 240;
+const int SCREEN_BPP = 32;
+
+
+SDL_Surface *buttonSheet = NULL;
+SDL_Surface *background = NULL;
+SDL_Surface *loading = NULL;
+SDL_Surface *screen = NULL;
+
+
+SDL_Surface *message = NULL;
+TTF_Font *font = NULL;
+SDL_Color textColor = { 255, 255, 255 };
+
+SDL_Event event;
+SDL_Rect Button_clips[ 4 ];
+SDL_Rect Loading_clips[ 8 ];
+
+sio::client socketio;
+
+
+
+void RGBtoBGR(SDL_Surface*);
+void handle_keys(SDL_Event* , int* );
+void apply_surface( int , int , SDL_Surface* , SDL_Surface* , SDL_Rect* );
+bool SDL_init();
+bool load_files();
+void clean_up();
+void set_clips();
+//void TFT_drawBitmap(int , int , FILE *, TFT *);
+
+char* get_IP(const char *iface){
+	
+	struct ifreq ifr;
+	int fd = socket(AF_INET, SOCK_DGRAM, 0);
+ 
+    //Type of address to retrieve - IPv4 IP address
+    ifr.ifr_addr.sa_family = AF_INET;
+ 
+    //Copy the interface name in the ifreq structure
+    strncpy(ifr.ifr_name , iface , IFNAMSIZ-1);
+ 
+    ioctl(fd, SIOCGIFADDR, &ifr);
+ 
+    close(fd);
+	return inet_ntoa(( (struct sockaddr_in *)&ifr.ifr_addr )->sin_addr);
+}
+
+
+class Button
+{
+    private:
+
+    SDL_Rect* clip;
+
+    public:
+    SDL_Rect box;
+    Button( int x, int y, int w, int h );
+
+    void handle_events();
+
+    void show();
+};
+Button::Button( int x, int y, int w, int h )
+{
+    box.x = x;
+    box.y = y;
+    box.w = w;
+    box.h = h;
+
+    clip = &Button_clips[ 0 ];
+}
+
+void Button::handle_events()
+{
+    int x = event.motion.x;
+    int y = event.motion.y;
+
+    if( event.type == SDL_MOUSEMOTION )
+    {
+        if( ( x > box.x ) && ( x < box.x + box.w ) && ( y > box.y ) && ( y < box.y + box.h ) )
+        {
+            clip = &Button_clips[ 2 ];
+        }
+        else
+        {
+            clip = &Button_clips[ 0 ];
+        }
+    }
+    if( event.type == SDL_MOUSEBUTTONDOWN )
+    {
+        if( event.button.button == SDL_BUTTON_LEFT )
+        {
+            if( ( x > box.x ) && ( x < box.x + box.w ) && ( y > box.y ) && ( y < box.y + box.h ) )
+            {
+				socketio.socket()->emit("water");
+                clip = &Button_clips[ 2 ];
+            }
+        }
+    }
+    if( event.type == SDL_MOUSEBUTTONUP )
+    {
+        if( event.button.button == SDL_BUTTON_LEFT )
+        {
+			socketio.socket()->emit("stopwater");
+			clip = &Button_clips[ 3 ];
+        }
+    }
+}
+
+void Button::show()
+{
+    apply_surface( box.x, box.y, buttonSheet, screen, clip );
+}
+
+void RGBtoBGR(SDL_Surface *image)
+{
+	int i, size;
+	unsigned char *data, swp;
+	data=(unsigned char*)image->pixels;
+
+	size=4*image->w*image->h;
+	for(i=0;i<size;i+=4)
+	{
+		swp=data[i];
+		data[i]=data[i+2];
+		data[i+2]=swp;
+	}
+}
+ 
+void handle_keys(SDL_Event* event, int* quit)
+{
+    if (event->type == SDL_KEYDOWN)
+    {
+		switch(event->key.keysym.sym){
+			case SDLK_ESCAPE:
+				*quit = 1;
+				break;
+			case SDLK_UP:
+				keyshift_y--;
+				break;
+			case SDLK_DOWN:
+				keyshift_y++;
+				break;
+			case SDLK_LEFT:
+				keyshift_x--;
+				break;
+			case SDLK_RIGHT:
+				keyshift_x++;
+				break;
+			case SDLK_w:
+				socketio.socket()->emit("water");
+				break;
+		}
+    }
+}
+
+void apply_surface( int x, int y, SDL_Surface* source, SDL_Surface* destination, SDL_Rect* clip = NULL )
+{
+    SDL_Rect offset;
+
+    offset.x = x;
+    offset.y = y;
+
+    SDL_BlitSurface( source, clip, destination, &offset );
+}
+
+bool SDL_init()
+{
+    if( SDL_Init( SDL_INIT_EVERYTHING ) == -1 )
+    {
+        return false;
+    }
+
+    screen = SDL_SetVideoMode( SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, SDL_SWSURFACE );
+
+    if( screen == NULL )
+    {
+        return false;
+    }
+
+    if( TTF_Init() == -1 )
+    {
+        return false;
+    }
+
+    SDL_WM_SetCaption( "TTF Test", NULL );
+
+    return true;
+}
+
+bool load_files()
+{
+    background = SDL_LoadBMP("background.bmp");;
+    loading = SDL_LoadBMP("loading.bmp");;
+    buttonSheet = SDL_LoadBMP( "water.bmp" );
+
+    font = TTF_OpenFont( "mvboli.ttf", 12 );
+
+
+    if( background == NULL )
+    {
+        return false;
+    }
+    if( loading == NULL )
+    {
+        return false;
+    }
+    if( font == NULL )
+    {
+        return false;
+    }
+	
+    if( buttonSheet == NULL )
+    {
+        return false;
+    }
+	
+    return true;
+}
+
+void clean_up()
+{
+    SDL_FreeSurface( background );
+    SDL_FreeSurface( loading );
+    SDL_FreeSurface( message );
+
+    TTF_CloseFont( font );
+
+    TTF_Quit();
+
+    SDL_Quit();
+}
+
+void set_clips()
+{
+	for(int j = 0; j < 2; j++){
+		for(int i = 0 ; i < 2; i++){
+			Button_clips[j*2+i].x = 47*i;
+			Button_clips[j*2+i].y = 47*j;
+			Button_clips[j*2+i].w = 47;
+			Button_clips[j*2+i].h = 47;
+		}
+	}
+	for(int j = 0; j < 2; j++){
+		for(int i = 0 ; i < 4; i++){
+			Loading_clips[j*4+i].x = 320*i;
+			Loading_clips[j*4+i].y = 240*j;
+			Loading_clips[j*4+i].w = 320;
+			Loading_clips[j*4+i].h = 240;
+		}
+	}
+}
+
+void rotation(float *x,float *y,float degree)
+{
+	float radian = degree * 0.017453;
+	float prex = *x,prey = *y;	
+	*x = prex*cos(radian)-prey*sin(radian);
+	*y = prex*sin(radian)+prey*cos(radian);
+	
+}
+
+void OnMessage(sio::event & val)
+{
+	ad[ad_pins_map[0]] = (float)val.get_message()->get_map()["Remaining"]->get_int() 	/ ad_magnification[0];
+	ad[ad_pins_map[1]] = (float)val.get_message()->get_map()["Humidity"]->get_int() 	/ ad_magnification[1];
+	ad[ad_pins_map[2]] = (float)val.get_message()->get_map()["etHumidity"]->get_int() 	/ ad_magnification[2];
+	ad[ad_pins_map[3]] = (float)val.get_message()->get_map()["Temperature"]->get_int()	/ ad_magnification[3];
+	ad[ad_pins_map[4]] = (float)val.get_message()->get_map()["Pressure"]->get_int() 	/ ad_magnification[4];
+}
+/*
+void TFT_drawBitmap(int x, int y, FILE *myFile, TFT *TFTdisp) {
+  if (myFile) {
+    unsigned long w = 0, h = 0;
+    char c = 0, R = 0, G = 0, B = 0;
+	for(int i = 0; i < 18 && c != EOF; i++)
+		c = getc(myFile);
+    for (int i = 0; i < 4 && c != EOF; i++)
+      w += (((c = getc(myFile)) & 0xFF) << (i * 8));
+    for (int i = 0; i < 4 && c != EOF; i++)
+      h += (((c = getc(myFile)) & 0xFF) << (i * 8));
+	for(int i = 0; i < 28 && c != EOF; i++)
+		c = getc(myFile);
+	
+	for (int j = h - 1; j >= 0; j--) {
+      for (int i = 0; i < w; i++){
+		B = getc(myFile);
+		G = getc(myFile);
+		R = getc(myFile);
+		TFTdisp->stroke(R, G, B);
+		TFTdisp->point(x+i, y+j);
+	  }
+      for (int i = 4; i > (w % 4) && (w % 4) != 0; i--)
+        getc(myFile);
+    } 
+  }
+}
+*/
+int main( int argc, char* args[] )
+{
+	printf("test");
+    int quit = 0;
+
+    if( SDL_init() == false )
+    {
+		printf("init fail\n");
+        return 1;
+    }
+
+    if( load_files() == false )
+    {
+		printf("load fail\n");
+        return 1;
+    }
+    Button myButton( 10, 183, 47, 47 );
+    set_clips();
+	SDL_FillRect( screen, &screen->clip_rect, SDL_MapRGB( screen->format, 0xFF, 0xFF, 0xFF ) );
+		
+	socketio.connect("http://localhost:3000");
+	ad_msg = TTF_RenderText_Solid( font, "Loading... Please wait", textColor );
+	for( int count = 0; !socketio.opened() && count < 300; count ++){
+		apply_surface( 0, 0, loading, screen, &Loading_clips[count % 8] );
+		apply_surface(20,115, ad_msg, screen );
+		
+		RGBtoBGR(screen);
+		SDL_Flip( screen );
+		usleep(100000);
+		
+	}
+	
+		
+	FILE *fp;
+	char usbwifiID[20];
+	fp = fopen("/tmp/usbwifiID", "r");
+	fscanf(fp, "%s", usbwifiID);
+	fclose(fp);
+    message = TTF_RenderText_Solid( font, get_IP(usbwifiID), (SDL_Color){ 255, 0, 0 } );
+
+	/*
+	TFT TFTscreen = TFT(cs, dc, rst);
+	TFTscreen.begin();
+	TFTscreen.background(0, 0, 0);
+	TFTscreen.fill(255, 0, 0);
+	TFTscreen.stroke(255, 0, 0);
+	TFTscreen.rect(0, 0, 160, 15);
+	TFTscreen.stroke(255, 255, 255);
+	TFTscreen.setTextSize(1);
+	TFTscreen.text(" < 86Duino plant on TFT > ", 0, 3);
+	TFTscreen.fill(0, 0, 255);
+	TFTscreen.stroke(0, 0, 255);
+	TFTscreen.rect(0, 114, 160, 15);
+	TFTscreen.stroke(255, 255, 255);
+	TFTscreen.setTextSize(1);
+	TFTscreen.text(get_IP("wlan3"), 40, 117);
+	FILE *file = fopen("plant3.bmp", "r");
+	TFT_drawBitmap(55, 15, file, &TFTscreen);
+
+	for (int i = 0; i < 4; i++) {
+		TFTscreen.stroke(0, 255, 0);
+		TFTscreen.text(TFT_name[i], 5 , 16 + i * 25);
+		TFTscreen.text(TFT_unit[i], 70 , 30 + i * 25);
+	}
+	char buffer[10];
+	*/
+    while( !quit )
+    {		
+
+/*
+		TFT_val[0] = ad[0]* ad_magnification[0];
+		TFT_val[1] = ad[2]* ad_magnification[2];
+		TFT_val[2] = ad[3]* ad_magnification[3];
+		TFT_val[3] = ad[4]* ad_magnification[4];
+		
+		TFTscreen.setTextSize(2);
+		for (int i = 0; i < 4; i++) {
+			if(TFT_val[i] == TFT_rval[i])
+				continue;
+			sprintf(buffer, "% 5d", TFT_rval[i]);
+			TFTscreen.stroke(0, 0, 0);
+			TFTscreen.text(buffer, 5 , 24 + i * 25);
+		}
+		for (int i = 0; i < 4; i++) {
+			sprintf(buffer, "% 5d", TFT_val[i]);
+			TFTscreen.stroke(0, 255, 0);
+			TFTscreen.text(buffer, 5 , 24 + i * 25);
+		}
+		
+		TFT_rval[0] = ad[0]* ad_magnification[0];
+		TFT_rval[1] = ad[2]* ad_magnification[2];
+		TFT_rval[2] = ad[3]* ad_magnification[3];
+		TFT_rval[3] = ad[4]* ad_magnification[4];
+*/
+
+
+
+		//keyshift
+		/*
+		char shift[10];
+		sprintf(shift,"%d",keyshift_x);
+		ad_msg = TTF_RenderText_Solid( font, shift, textColor );
+		apply_surface(10,50, ad_msg, screen );
+		
+		sprintf(shift,"%d",keyshift_y);
+		ad_msg = TTF_RenderText_Solid( font, shift, textColor );
+		apply_surface(30,50, ad_msg, screen );
+		*/
+        apply_surface( 0, 0, background, screen );
+		apply_surface( 215, 225, message, screen );
+        myButton.show();
+		
+		
+		short homex = 180,homey = 108;
+		/*
+		for(int i = 0; i < 5; i++)
+			ad[ad_pins_map[i]] = (float)analogRead(A0+i)/1024;
+		
+		*/
+		socketio.socket()->on("update", &OnMessage);
+		
+		
+		
+		for(int i = 0; i < 5; i++){
+	
+			float x = 0,y = 78;
+			float x2 = 0,y2 = 88;
+			float avg1,avg2;
+			short output_x[4];
+			short output_y[4];
+			char s[20];
+			avg1 = i!=0 ? (ad[i]+ad[i-1])/4 : (ad[0]+ad[4])/4;
+			avg2 = i!=4 ? (ad[i]+ad[i+1])/4 : (ad[0]+ad[4])/4;
+			
+			rotation(&x2,&y2,-180-72*i-36);
+			sprintf(s,"%d",(int)(ad[i] * ad_magnification[i]));
+			ad_msg = TTF_RenderText_Solid( font, s, (SDL_Color){ 255, 64, 64 } );
+			apply_surface((x2*ad[i]) +homex -9,(y2*ad[i]) +homey  -4, ad_msg, screen );
+			
+						
+			rotation(&x,&y,-180-72*i);
+			output_x[0] = (x*avg1) + homex;
+			output_y[0] = (y*avg1) + homey;
+			rotation(&x,&y,-36);
+			output_x[1] = (x*ad[i]) + homex;
+			output_y[1] = (y*ad[i]) + homey;
+			rotation(&x,&y,-36);
+			output_x[2] = (x*avg2) + homex;
+			output_y[2] = (y*avg2) + homey;
+			
+			output_x[3] = homex;
+			output_y[3] = homey;
+
+			filledPolygonRGBA(	screen, 
+								output_x, output_y,
+								4,
+								(1.0-(float)ad[i])*255, (float)ad[i]*255, 0, 128);
+			
+		}
+		
+        while(SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+            {
+                quit = 1;
+            }
+            handle_keys(&event, &quit);
+			
+            myButton.handle_events();
+        }
+		
+		RGBtoBGR(screen);
+		SDL_Flip( screen );
+		
+		
+		
+		usleep(500000);
+		
+    }
+
+    clean_up();
+
+    return 0;
+}
+
+
